d225a87f9c16299536f3995ca01a6950488a4d57
Rohan <rohansain918@gmail.com>
2026-08-16 20:00:55 +0530

    new passwordgen

--- passwordgen.py
+++ passwordgen.py
@@ -1,12 +1,5 @@
 import random;
-
 # inp = random.choices("!@#$_*", k=1)
-
-punc1 = random.choices("!@#$_*", k=2)
-punc2 = random.choices("!@#$_*", k=2)
-num1 = random.choices("1234567890", k=2)
-num2 = random.choices("1234567890", k=4)
-num3 = random.choices("1234567890", k=1)
-
-password =  "".join(num2) + "".join(punc1) + "".join(num1) + "".join(punc2) + "".join(num3)
+punc1 = random.choices("!@#$_*qwertyuiopasdfghjklzxcvbnm0123456789", k=8)
+password =  "".join(punc1) 
 print(password)
